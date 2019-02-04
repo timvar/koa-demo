6306f74592f4b0d5d81d26e663c0268728c259cf
Timo Varila <timo.varila@myy.haaga-helia.fi>
2019-02-04 21:38:16 +0200

    demo v1

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const Koa = require('koa');
-
+const cors = require('@koa/cors');
 const app = new Koa();
+app.use(cors());
 
-app.use(async ctx => ctx.body = { msg: 'Hello World' });
+app.use(async ctx => { 
+  
+  /*ctx.body = { msg: 'Hello World!' }}); */
 
+ 
+  switch (ctx.url) {
+    case '/eng':
+      ctx.body = { msg: 'Hello World!' };
+      break;
+
+    case '/fin':
+      ctx.body = { msg: 'Morjens Maailma!' };
+      break;
+
+    case '/swe':
+      ctx.body = { msg: 'Hejssan Värld!' };
+      break;
+
+    case '/ger':
+      ctx.body = { msg: 'Guten Tag Welt!' };
+      break;
+
+    default: 
+      ctx.body = { msg: 'Vastaan nyt jotain kun en muutakaan osaa...' };
+      break;
+  }});
+ 
 app.listen(3000, () => console.log('Server started...'));
